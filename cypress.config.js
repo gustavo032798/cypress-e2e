3b80f17dda1f74ee1a474793cc3e0803143378df
gustavo.crc <gustavo.carranca1@hotmail.com>
2025-03-18 23:31:30 -0300

    Ajuste CI/CD e Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://notes-serverless-app.com/',
     env: {
       viewportWidthBreakpoint: 768,
     },
     defaultCommandTimeout: 20000,
     chromeWebSecurity: false,
+    projectId: 'b2rfe7',
   },
 })
